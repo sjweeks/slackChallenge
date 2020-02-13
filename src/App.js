11151bf595905252fe2ec0a94cc741b3d2240cc6
Code Nation <codenation@Codes-MacBook-Pro.local>
2020-02-13 17:07:39 +0000

    moved message to Content

--- src/App.js
+++ src/App.js
@@ -7,27 +7,7 @@ import ContentHeader from './components/ContentHeader.js'
 
 class App extends Component {
 
-  state = {
-    inputValue: "",
-    message: []
-  }
-
-  updateMessage = (e) => {
-    this.setState({
-      inputValue: e.target.value
-    })
-  }
-
-  storeMessage = () => {
-    let joined = this.state.message.concat(this.state.inputValue)
-    this.setState({
-      message: joined
-    })
-  }
-
   render() {
-
-    const messages = this.state.message.map(item => <p>{item}</p> );
 
     return (
       <div>
@@ -40,9 +20,6 @@ class App extends Component {
         <div className='content'>
           <ContentHeader />
           <Content />
-          {messages}
-          <input type="text" onChange={this.updateMessage}/>
-          <button onClick={this.storeMessage}>Send Message</button>
         </div>
         
       </div>
--- src/components/Content.js
+++ src/components/Content.js
@@ -1,11 +1,48 @@
-import React from 'react'
+import React, { Component } from 'react'
+import '../css/Content.css'
 
-function Content(props) {
-    return (
-        <div>
-            
-        </div>
-    )
+class Content extends Component {
+
+    state = {
+        profilePic: "",
+        userName: "",
+        messageTime: "",
+        inputValue: "",
+        message: []
+    }
+
+    updateMessage = (e) => {
+        this.setState({
+            inputValue: e.target.value
+        })
+    }
+
+    storeMessage = () => {
+        let joined = this.state.message.concat(this.state.inputValue)
+        this.setState({
+            message: joined
+        })
+    }
+
+    render() {
+
+        const messages = this.state.message.map(item => <p>{item}</p>, <p>"16:00"</p>);
+
+        return ( 
+            <div className="messageBody">
+
+                <div>
+                    {/* UserImage, UserName, TimeSent */}
+                    {messages}
+                </div>
+                <div className="inputArea">
+                    <input type="text" onChange={this.updateMessage}/>
+                    <button onClick={this.storeMessage}>Send Message</button>
+                </div>
+                
+            </div>
+        )
+    }
 }
 
 export default Content;
