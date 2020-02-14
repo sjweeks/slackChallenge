ee672d73bc2b132b73025cf42d8a40d07a565c1a
Sarah Weeks <sarahweeks@Sarahs-MacBook-Pro.local>
2020-02-14 10:28:00 +0000

    Latest Commit - CSS and Images added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React, { Component } from 'react'
-import './css/App.css';
+import React, { Component } from "react";
+import "./css/App.css";
 
-import Content from './components/Content.js'
-import Navbar from './components/Navbar.js'
-import ContentHeader from './components/ContentHeader.js'
+import Content from "./components/Content.js";
+import Navbar from "./components/Navbar.js";
+import ContentHeader from "./components/ContentHeader.js";
+import Attach from './images/attach.png';
+import Bold from './images/bold.png';
+import Italic from './images/italic.png';
+import StrikeThrough from './images/strikethrough.png';
+import Link from './images/link.png';
+import OrderList from './images/orderlist.png';
+import List from './images/list.png';
+import Type from './images/type.png';
+import At from './images/at.png';
+import Emoji from './images/smile.png';
 
 class App extends Component {
-
   render() {
-
     return (
       <div className="container">
         <div className="nav-container">
@@ -19,11 +27,15 @@ class App extends Component {
         <div className="content-container">
           <ContentHeader />
           <Content />
-          <input className="inputSection" type='text'/>
+{/* 
+          <div c>
+          <input className="inputSection" type="text" />
+            <img src={Attach}/>
+
+          </div> */}
         </div>
-        
       </div>
-    )
+    );
   }
 }
 
